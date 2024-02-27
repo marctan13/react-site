d60ab39323bd322ca19860e5bbba93a2c5838953
marc-tan <marctan13@gmail.com>
2024-02-26 18:23:09 -0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <img src='react-icon-small.png' className='nav--icon'/>
+      <h3 className='nav--logo_text'>ReactFacts</h3>
+      <h3 className='nav--title'>React Course - Project 1</h3>
+    </nav>
+  )
+}
+
+export default Navbar
+
+
